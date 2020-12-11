a200c194c5a46b1a590ca820fa27b66db54e1cd8
Sindre Wilting <sindrewilting@Sindres-MacBook-Pro.local>
2020-12-11 12:49:52 +0100

    Use the tenant map to get the correct display name

--- Source/Login/Backend/identities/tenants/getter.go
+++ Source/Login/Backend/identities/tenants/getter.go
@@ -4,15 +4,26 @@ type Getter interface {
 	GetTenantFromID(tenantID TenantID) (*Tenant, error)
 }
 
-func NewGetter() Getter {
-	return &getter{}
+func NewGetter(configuration Configuration) Getter {
+	return &getter{
+		configuration,
+	}
 }
 
-type getter struct{}
+type getter struct {
+	configuration Configuration
+}
 
+// Use tenant mapper
 func (g *getter) GetTenantFromID(tenantID TenantID) (*Tenant, error) {
+	tenantNamesMap := g.configuration.TenantNamesMap()
+	displayName := string(tenantID)
+	if value, ok := (*tenantNamesMap)[tenantID]; ok {
+		displayName = value
+	}
+
 	return &Tenant{
 		ID:      tenantID,
-		Display: string(tenantID),
+		Display: displayName,
 	}, nil
 }
