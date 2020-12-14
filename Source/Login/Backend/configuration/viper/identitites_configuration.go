86fa2c1fa4f35a8655c5a7d6f4b234fe0a90ca86
Sindre Alstad Wilting <woksin@users.noreply.github.com>
2020-12-14 09:31:17 +0100

    Update Source/Login/Backend/configuration/viper/identitites_configuration.go
    Co-authored-by: Jakob Høgenes <1014990+jakhog@users.noreply.github.com>

--- Source/Login/Backend/configuration/viper/identitites_configuration.go
+++ Source/Login/Backend/configuration/viper/identitites_configuration.go
@@ -7,7 +7,7 @@ import (
 
 const (
 	identitiesCurrentUserCookieNameKey = "identities.cookie_name"
-	identitiesTenantNameMapKey         = "identities.tenants"
+	identitiesTenantNamesKey         = "identities.tenants"
 
 	defaultIdentitiesCurrentUserCookieNameValue = "ory_kratos_session"
 )
