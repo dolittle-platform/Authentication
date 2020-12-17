6c603490e50540c7fda28d626abfb64db4152004
woksin <sindrewilting@gmail.com>
2020-12-17 14:47:11 +0100

    empty string url when no image url configured for provider

--- Source/Login/Web/select-provider/IdentityProviderListItem.tsx
+++ Source/Login/Web/select-provider/IdentityProviderListItem.tsx
@@ -26,7 +26,7 @@ export class IdentityProviderListItem extends React.Component<IdentityProviderLi
                 <DocumentCardPreview previewImages={[
                     {
                         // previewImageSrc: 'https://docs.microsoft.com/en-us/azure/active-directory/develop/media/howto-add-branding-in-azure-ad-apps/ms-symbollockup_mssymbol_19.svg',
-                        previewImageSrc: this.props.provider.imageURL || 'https://docs.microsoft.com/en-us/azure/active-directory/develop/media/howto-add-branding-in-azure-ad-apps/ms-symbollockup_mssymbol_19.svg',
+                        previewImageSrc: this.props.provider.imageURL || '',
                         width: 60,
                         height: 60,
                     }
