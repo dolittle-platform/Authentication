9096f505acbd595363ce8e261257f0bd73f19fc5
Jakob Høgenes <1014990+jakhog@users.noreply.github.com>
2021-07-01 13:49:49 +0200

    Allow configuration of what token to use

--- Source/Pascal/openid/config/configuration.go
+++ Source/Pascal/openid/config/configuration.go
@@ -2,10 +2,18 @@ package config
 
 import "net/url"
 
+type TokenType string
+
+const (
+	AccessToken TokenType = "access_token"
+	IDToken     TokenType = "id_token"
+)
+
 type Configuration interface {
 	Issuer() *url.URL
 	ClientID() string
 	ClientSecret() string
 	Scopes() []string
+	TokenType() TokenType
 	RedirectURL() *url.URL
 }
