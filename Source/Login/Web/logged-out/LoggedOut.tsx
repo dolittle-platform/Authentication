7a3adb377539b2ff82306ffb3251b8ddd4e2b621
N00bG1rl <403helen@gmail.com>
2022-07-14 18:24:47 +0300

    Changed 'Logged out' page content and styles

--- Source/Login/Web/logged-out/LoggedOut.tsx
+++ Source/Login/Web/logged-out/LoggedOut.tsx
@@ -1,17 +1,35 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+import { Box, Button, Typography } from '@mui/material';
+import { ArrowBack } from '@mui/icons-material';
+import { Theme } from '../styles/Theme';
 
 import { configuration } from '../Configuration';
-import { Theme } from '../styles/Theme';
+import { LoginWrapper } from '../layouts/LoginWrapper';
+
+const styles = {
+    letterSpacing: '0.06em',
+    transition: 'all .3s',
+    '&:hover': {
+        color: Theme.palette.primary.light
+    }
+}
 
 export const LoggedOut = (): JSX.Element => {
     return (
-        <>
-            <Typography variant='h2'>You have been logged out.</Typography>
-            <Button size='large' color='inherit' href={configuration.startPath} css={{ marginTop: '128px', [Theme.breakpoints.up('sm')]: { marginTop: '25px' } }}>Go back and log in again</Button>
-        </>
+        <LoginWrapper>
+            <Typography variant='h2' sx={{ letterSpacing: '-0.5px' }}>You have been logged out.</Typography>
+            <Box mt={13.5}>
+                <Button
+                    size='large'
+                    color='inherit'
+                    startIcon={<ArrowBack />}
+                    sx={styles}
+                    href={configuration.startPath}>
+                    Return to login page
+                </Button>
+            </Box>
+        </LoginWrapper>
     );
 }
