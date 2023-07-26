3d51c076a9c0042f7f84424c4a4f2f57d3be0bf2
N00bG1rl <403helen@gmail.com>
2023-07-26 05:00:23 +0300

    Change SelectProvider appearance

--- Source/Login/Web/select-provider/SelectProvider.tsx
+++ Source/Login/Web/select-provider/SelectProvider.tsx
@@ -2,23 +2,22 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 import { Suspense } from 'react';
-import { Link } from 'react-router-dom';
 
-import { Box, CircularProgress, Divider, Link as MuiLink, Typography } from '@mui/material';
+import { Box, CircularProgress, Link, Typography } from '@mui/material';
 
 import { configuration } from '../Configuration';
 import { SelectProviderForm } from './SelectProviderForm';
 import { WelcomeHeader } from '../components/WelcomeHeader';
 
 export const SelectProvider = () => {
-    const { showDolittleHeadline } = configuration;
+    const { showDolittleHeadline, supportEmail } = configuration;
 
     return (
         <>
             <Box sx={{ maxWidth: 370, mb: 12.25, mx: 'auto' }}>
                 <WelcomeHeader />
 
-                <Typography variant='h5'>
+                <Typography variant='h5' sx={{ mt: 4 }}>
                     {showDolittleHeadline ? 'Transform your business by leveraging real time events.' : 'Sign in to continue.'}
                 </Typography>
             </Box>
@@ -27,10 +26,8 @@ export const SelectProvider = () => {
                 <SelectProviderForm />
             </Suspense>
 
-            <Divider variant='middle' sx={{ my: 4, backgroundColor: '#3B3D48' }} />
-
-            <Typography variant='body2' color='gray'>
-                Don't have an account? <MuiLink component={Link} to='/.auth/no-tenant'>Get access</MuiLink>
+            <Typography sx={{ mt: 4 }}>
+                Don't have an account? <Link href={'mailto:' + supportEmail}>Contact us</Link> to get started.
             </Typography>
         </>
     );
